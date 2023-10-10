4513ca11e94af8b569bf5884bc6e29dbb985bd13
Şeta Narut <36481442+setanarut@users.noreply.github.com>
2023-10-11 01:36:19 +0300

    Update bezier.go

--- bezier.go
+++ bezier.go
@@ -1,12 +1,22 @@
 package gog
 
-type CubicBezier struct {
-	a, b, c, d Point
+type cubicBezier struct {
+	pts [4]Point
 }
 
 // GetPoint returns point at time t
-func (cb *CubicBezier) GetPoint(t float64) Point {
-	x := (1-t)*(1-t)*(1-t)*cb.a.X + 3*(1-t)*(1-t)*t*cb.b.X + 3*(1-t)*t*t*cb.c.X + t*t*t*cb.d.X
-	y := (1-t)*(1-t)*(1-t)*cb.a.Y + 3*(1-t)*(1-t)*t*cb.b.Y + 3*(1-t)*t*t*cb.c.Y + t*t*t*cb.d.Y
+func (cb *cubicBezier) getPoint(t float64) Point {
+	x := (1-t)*(1-t)*(1-t)*cb.pts[0].X + 3*(1-t)*(1-t)*t*cb.pts[1].X + 3*(1-t)*t*t*cb.pts[2].X + t*t*t*cb.pts[3].X
+	y := (1-t)*(1-t)*(1-t)*cb.pts[0].Y + 3*(1-t)*(1-t)*t*cb.pts[1].Y + 3*(1-t)*t*t*cb.pts[2].Y + t*t*t*cb.pts[3].Y
 	return Point{X: x, Y: y}
+}
+
+// flatten returns flattan Bezier
+func (cb *cubicBezier) flatten(samples int) []Point {
+
+	coords := make([]Point, 0)
+	for _, t := range Linspace(0, 1, samples) {
+		coords = append(coords, cb.getPoint(t))
+	}
+	return coords
 }
