94f4a1ae0685b33dd0289d0558b3dd4ab5289cc7
zolt <duurkak@yandex.com>
2023-10-16 03:20:20 +0300

    fix variable name

--- README.md
+++ README.md
@@ -43,11 +43,11 @@ func main() {
 
 ```go
 rect := gog.Rect(gog.Point{}, 30, 10)
-poly := gog.Circle(c.Center, 50).Scale(gog.P(1, 0.5))
+ellipse := gog.Circle(c.Center, 50).Scale(gog.P(1, 0.5))
 for _, t := range gog.Linspace(0, 1, 150) {
 	c.Clear(gog.Black)
-	poly.Stroke(c)
-	p, a := poly.PointAngleAtTime(t)
+	ellipse.Stroke(c)
+	p, a := ellipse.PointAngleAtTime(t)
 	rect.SetPos(p).Rotated(a).SetFill(colornames.Yellow).Fill(c)
 	c.AppendAnimationFrame()
 }
--- examples/point_angle/point_angle.go
+++ examples/point_angle/point_angle.go
@@ -8,11 +8,11 @@ import (
 func main() {
 	c := gog.New(200, 100)
 	rect := gog.Rect(gog.Point{}, 30, 10)
-	poly := gog.Circle(c.Center, 50).Scale(gog.P(1, 0.5))
+	ellipse := gog.Circle(c.Center, 50).Scale(gog.P(1, 0.5))
 	for _, t := range gog.Linspace(0, 1, 150) {
 		c.Clear(gog.Black)
-		poly.Stroke(c)
-		p, a := poly.PointAngleAtTime(t)
+		ellipse.Stroke(c)
+		p, a := ellipse.PointAngleAtTime(t)
 		rect.SetPos(p).Rotated(a).SetFill(colornames.Yellow).Fill(c)
 		c.AppendAnimationFrame()
 	}
