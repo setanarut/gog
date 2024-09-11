31e054343e7fc006f5af49a23aa3988dc7b93a56
nalaynid <duurkak@yandex.com>
2024-09-11 15:42:38 +0300

    path follow example

--- examples/path_follow/main.go
+++ examples/path_follow/main.go
@@ -1,32 +1,25 @@
 package main
 
 import (
-	"fmt"
+	"image/color"
 
 	"github.com/setanarut/gog/v2"
 	"github.com/setanarut/gog/v2/shapes"
+	"github.com/setanarut/gog/v2/utils"
+	"github.com/setanarut/vec"
 )
 
 func main() {
-	// ctx := gog.NewContext(200, 100)
-	// rect := shapes.Rect(vec.Vec2{}, 30, 10)
-	// lemn := shapes.Lemniscate(100, 100)
-	// lemnTotalLength := lemn.Length()
-	// for _, length := range utils.Linspace(0, lemnTotalLength, 120) {
-	// 	ctx.Clear(color.Black)
-	// 	ctx.Stroke(lemn, gog.DefaultStrokeStyle())
-	// 	pos, ang := lemn.PointAngleAtLength(length)
-	// 	ctx.Fill(rect.SetPos(pos).Rotated(ang), color.White)
-	// 	ctx.AppendAnimationFrame()
-	// }
-	// ctx.SaveAPNG("point_angle.png", 2)
-	drawim()
-}
-func drawim() {
-	ctx := gog.NewContext(400, 200)
-	lemn := shapes.Lemniscate(50, 173).SetPos(ctx.Center)
-	fmt.Println("a", lemn.Anchor)
-	fmt.Println(lemn.Centroid())
-	ctx.DebugDraw(lemn)
-	ctx.SavePNG("point_angle.png")
+	ctx := gog.NewContext(250, 100)
+	rect := shapes.Rect(vec.Vec2{}, 30, 10)
+	lemn := shapes.Lemniscate(100, 100).SetPos(ctx.Center)
+	lemnTotalLength := lemn.Length()
+	for _, length := range utils.Linspace(0, lemnTotalLength, 120) {
+		ctx.Clear(color.Black)
+		ctx.Stroke(lemn, gog.DefaultStrokeStyle())
+		pos, ang := lemn.PointAngleAtLength(length)
+		ctx.Fill(rect.SetPos(pos).Rotated(ang), color.White)
+		ctx.AppendAnimationFrame()
+	}
+	ctx.SaveAPNG("path_follow.png", 2)
 }
